ebc115fe7490f696b3a532f2a59c3d00a7c64ed1
Ryan McCrary <rrmccrary@gmail.com>
2013-11-17 21:56:47 -0500

    Messin

--- app.rb
+++ app.rb
@@ -9,6 +9,10 @@ get '/hello/:name' do
 	"Hello #{params[:name]}"
 end
 
+get '/i/ran/:miles' do
+   "Well dang, #{params[:miles]} is a lot of miles. You burned off ((#{params[:miles]} * 140 ) / 120) High Lifes. Drink up!"
+end
+
 not_found do
 	status 404
 	'not found'
